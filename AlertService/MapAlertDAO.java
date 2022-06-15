e6b8fc41d8a22235e90bb80eeb88599641a390d2
rabosian <msann1356@gmail.com>
2022-06-14 20:28:10 -0400

    fix typo

--- AlertService/MapAlertDAO.java
+++ AlertService/MapAlertDAO.java
@@ -5,15 +5,17 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
-public class MapAlertDAO {
+public class MapAlertDAO implements AlertDAO {
     private final Map<UUID, Date> alerts = new HashMap<UUID, Date>();
 	
+    @Override
     public UUID addAlert(Date time) {
     	UUID id = UUID.randomUUID();
         this.alerts.put(id, time);
         return id;
     }
-	
+    
+	@Override
     public Date getAlert(UUID id) {
         return this.alerts.get(id);
     }
